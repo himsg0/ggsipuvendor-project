014efdd293ae5e71ff4c5a02778a9d6383a08b17
himdee <himdee7@gmail.com>
2022-06-08 22:08:46 +0530

    initial commit

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Connection = async (URL) => {
+    // const URL = `mongodb://${username}:${password}@ecommerceweb-shard-00-00.fdvft.mongodb.net:27017,ecommerceweb-shard-00-01.fdvft.mongodb.net:27017,ecommerceweb-shard-00-02.fdvft.mongodb.net:27017/ECOMMERCE?ssl=true&replicaSet=atlas-8a6bhp-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    
+    try {
+        await mongoose.connect("mongodb://localhost:27017", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Error: ', error.message);
+    }
+
+};
+
+export default Connection;
